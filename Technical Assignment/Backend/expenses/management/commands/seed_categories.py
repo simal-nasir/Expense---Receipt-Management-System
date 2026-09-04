249ad0712d80445877de761abef0f78ef3c0173a
simal-nasir <simalnasir1@gmail.com>
2026-09-04 23:46:06 +0500

    Add automatic admin user creation

--- Technical Assignment/Backend/expenses/management/commands/seed_categories.py
+++ Technical Assignment/Backend/expenses/management/commands/seed_categories.py
@@ -1,11 +1,15 @@
 from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
 from expenses.models import Category
+import os
 
 
 class Command(BaseCommand):
-    help = "Create default expense categories"
+    help = "Create default categories and admin user"
 
     def handle(self, *args, **kwargs):
+
+        # Create default categories
         categories = [
             "Food",
             "Transport",
@@ -21,5 +25,49 @@ class Command(BaseCommand):
             Category.objects.get_or_create(name=name)
 
         self.stdout.write(
-            self.style.SUCCESS("Default categories created successfully.")
+            self.style.SUCCESS(
+                "Default categories created successfully."
+            )
         )
+
+        # Create admin user
+        User = get_user_model()
+
+        username = os.getenv("ADMIN_USERNAME")
+        email = os.getenv("ADMIN_EMAIL")
+        password = os.getenv("ADMIN_PASSWORD")
+
+        if username and email and password:
+
+            user, created = User.objects.get_or_create(
+                username=username,
+                defaults={
+                    "email": email,
+                },
+            )
+
+            user.email = email
+            user.is_staff = True
+            user.is_superuser = True
+            user.set_password(password)
+            user.save()
+
+            if created:
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f"Admin user '{username}' created successfully."
+                    )
+                )
+            else:
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f"Admin user '{username}' updated successfully."
+                    )
+                )
+
+        else:
+            self.stdout.write(
+                self.style.WARNING(
+                    "Admin environment variables are not configured."
+                )
+            )
